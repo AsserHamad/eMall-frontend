9a63694772d17672eacbf141e5a9012fea368490
Asser Hamad <asserhamad@gmail.com>
2020-12-17 02:41:15 +0200

    changes to client login

--- global.style.js
+++ global.style.js
@@ -2,6 +2,7 @@ exports.gStyles =  {
   primary: '#E74A5F',
   secondary: '#005071',
   background: '#F1FAEE',
+  lightPrimary: '#FF9E9E',
   tint: 'rgba(231, 74, 95, 0.4)',
   fontSizeL: 16,
   fontSizeM: 14,
--- screens/Authentication/ClientLogin.js
+++ screens/Authentication/ClientLogin.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { Button, Image, StyleSheet, Text, View } from 'react-native';
-import { TextInput } from 'react-native-gesture-handler';
+import { Button, Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+import { TextInput, TouchableHighlight, TouchableOpacity } from 'react-native-gesture-handler';
 import { gStyles } from '../../global.style';
 import Constants from 'expo-constants';
 
+const [width, height] = [Dimensions.get('window').width, Dimensions.get('window').height]
 const ClientLogin = (props) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -17,58 +18,127 @@ const ClientLogin = (props) => {
             if(res.ok) return res.json();
             else console.log(res.json());
         })
-        .then(res => console.log(res))
+        .then(res => setEmail(res.token))
         .catch(err => console.log(err))
     }
     return (
         <View style={styles.container}>
-            <Image style={styles.image} source={require('../../assets/logoM.png')} />
+            <View style={styles.backContainer}>
+                <TouchableOpacity onPress={() => props.navigation.navigate('Home')}>
+                    <Text>Back</Text>
+                </TouchableOpacity>
+            </View>
+            <Image style={styles.image} source={require('../../assets/logo.png')} />
             <View style={styles.headerContainer}>
-                <Text style={{color: 'white', fontSize: 23}}>Welcome Back!</Text>
-                <Text style={{color: 'white', fontSize: 13, fontWeight: 'light'}}>Please login to continue shopping</Text>
+                <Text style={{color: gStyles.secondary, fontSize: 23}}>Welcome Back!</Text>
+                <Text style={{color: gStyles.secondary, fontSize: 13}}>Please login to continue shopping</Text>
             </View>
-            <View>
-                <TextInput value={email} placeholder={'Email'} onChangeText={(val) => setEmail(val)} style={styles.input} />
-                <TextInput value={password} placeholder={'Password'} onChangeText={(val) => setPassword(val)} style={styles.input} />
+            <View style={styles.formContainer}>
+                <TextInput 
+                    value={email}
+                    textContentType={"emailAddress"}
+                    autoCompleteType={"email"}
+                    placeholder={'Email'}
+                    placeholderTextColor={"#ffc6c6"}
+                    onChangeText={(val) => setEmail(val)}
+                    style={styles.input} />
+                <TextInput 
+                    value={password}
+                    textContentType={"password"}
+                    autoCompleteType={"password"}
+                    placeholder={'Password'}
+                    placeholderTextColor={"#ffc6c6"}
+                    secureTextEntry={true}
+                    onChangeText={(val) => setPassword(val)}
+                    style={styles.input} />
+                <TouchableOpacity>
+                    <Text style={{color: gStyles.primary}}>Forgot Password</Text>
+                </TouchableOpacity>
             </View>
-            <Button onPress={() => login()} title={'Submit'} />
+            <TouchableOpacity onPress={() => login()}>
+                <View style={styles.submitButton}>
+                    <Text style={{color: 'white'}}>SUBMIT</Text>
+                </View>
+            </TouchableOpacity>
+            <View style={styles.others}>
+                {/* Register Now */}
+                <View style={{display:'flex', flexDirection: 'row'}}>
+                    <Text style={{color: gStyles.secondary}}>Don't have an account?</Text>
+                    <TouchableOpacity><Text style={{marginLeft: 5, color: gStyles.primary}}>Sign Up Now</Text></TouchableOpacity>
+                </View>
+                {/* Seller Account */}
+                <View style={{display:'flex', flexDirection: 'row', marginTop: 5}}>
+                    <Text style={{color: gStyles.secondary}}>Are you a seller?</Text>
+                    <TouchableOpacity><Text style={{marginLeft: 5, color: gStyles.primary}}>Login Here</Text></TouchableOpacity>
+                </View>
+            </View>
+                {/* Other Logins */}
+                <View style={styles.alternativeLogins}>
+
+                </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: gStyles.primary,
+        backgroundColor: gStyles.background,
         height: '100%',
-        // justifyContent: 'center',
         alignItems: 'center',
-        paddingTop: 80,
+        paddingTop: Constants.statusBarHeight,
         display: 'flex',
         flexDirection: 'column'
     },
+    backContainer: {
+        width,
+        marginTop: 20,
+        paddingHorizontal: 20
+    },
     imageContainer: {
-        width: 120,
-        height: 120,
-        borderColor: gStyles.secondary,
-        borderWidth: 4,
-        backgroundColor: gStyles.background,
-        borderRadius: 100,
+        width: '100%',
+        height: 100,
         justifyContent: 'center',
         alignItems: 'center'
     },
     image: {
-        height: 100,
-        width: 100
+        height: 90,
+        aspectRatio: 1875/870
     },
     headerContainer: {
-        width: '100%',
+        width: width * 0.9,
         paddingTop: 30,
-        paddingHorizontal: 20
+        // paddingHorizontal: 20,
+        marginBottom: 30
+    },
+    formContainer: {
+        width: width * 0.9
     },
     input: {
-        width: 200,
+        // width: width * 0.85,
+        fontSize: 18,
+        marginVertical: 10,
         height: 40,
-        borderWidth: 2
+        borderBottomWidth: 2,
+        borderColor: '#707070',
+    },
+    submitButton: {
+        backgroundColor: gStyles.primary,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: width * 0.9,
+        height: 45,
+        borderRadius: 2,
+        marginTop: 40
+    },
+    others: {
+        width: width * 0.9,
+        marginTop: 150
+    },
+    alternativeLogins: {
+        backgroundColor: gStyles.secondary,
+        width,
+        height: 500,
+        marginTop: 20
     }
 })
 
